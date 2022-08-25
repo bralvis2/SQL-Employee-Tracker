e15a14777fbdf4e61576daba689c215379ee3093
Brenda Alvis <brendaalvis@brendas-mbp-2.lan>
2022-08-24 20:24:09 -0500

    Set up Inquirer prompts

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const inquirer = require('inquirer');
-// import and require mysql2
+// import and require mysql2 & cTable
 const mysql = require('mysql2');
+const cTable = require('console.table');
 
 
 // Express Middleware
@@ -15,9 +16,51 @@ const db = mysql.createConnection({
     user: 'root',
     // mysql password
     password: 'rootroot',
-    database: 'db',
+    database: 'organization_db',
+});
+
+// initialize inquirer prompts
+const startMenu = () => {
+  inquirer.prompt({
+    type: "list",
+    name: "choice",
+    message: "What would you like to do?",
+    choices: [
+      "View All Employees",
+      "View All Employees By Department",
+      "View All Employees By Manager",
+      "Add Employee",
+      "Remove Employee",
+      "Update Employee Role",
+      "Update Employee Manager",
+    ]
+  })
+  .then((answer) => {
+    if(answer.choice === "View All Employees"){
+      viewAllEmployees();
+    }
+    if(answer.choice === "View All Employees By Department"){
+      employeesByDepartment();
+    }
+    if(answer.choice === "View All Employees By Manager"){
+      employeesByManager();
+    }
+    if(answer.choice === "Add Employee"){
+      addEmployee();
+    }
+    if(answer.choice === "Remove Employee"){
+      removeEmployee();
+    }
+    if(answer.choice === "Update Employee Role"){
+      updateEmployeeRole();
+    }
+    if(answer.choice === "Update Employee Manager"){
+      updateEmployeeManager();
+    }
+  })
 }
-);
+
+startMenu();
 
 app.use((req, res) => {
     res.status(404).end();
